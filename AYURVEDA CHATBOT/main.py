de97e96b97f64f1685bc62b112ebc734fea5ce11
Advait Chandorkar <77112795+advaitchandorkar@users.noreply.github.com>
2025-01-23 15:46:17 +0530

    Update main.py

--- AYURVEDA CHATBOT/main.py
+++ AYURVEDA CHATBOT/main.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, jsonify, request
 from flask_pymongo import PyMongo
 import openai
 
-openai.api_key = "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"
+openai.api_key = "********************************"
 
 
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://advaitDB:advait0612@cluster0.p9fan.mongodb.net/chatgpt"
+app.config["MONGO_URI"] = "**************************************"
 mongo = PyMongo(app)
 
 @app.route("/")
